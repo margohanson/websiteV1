887f444979f81b55c870b01e7e63f01b34ce69a1
Jordi Ferrer Orri <9082687+jordiferrero@users.noreply.github.com>
2025-11-23 12:22:16 +0100

    self host

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://margohanson.github.io/websiteV1/',
+  site: 'http://www.arianemader.com',
   integrations: [
     tailwind(),
     sitemap(),
