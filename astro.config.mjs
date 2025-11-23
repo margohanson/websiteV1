9fe89722fc47e7c74fc9506ccb0bea24cd7c233a
Jordi Ferrer Orri <9082687+jordiferrero@users.noreply.github.com>
2025-11-23 11:01:44 +0100

    paths broken again

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,8 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://margohanson.github.io/websiteV1',
+  site: 'https://margohanson.github.io',
+  base: '/websiteV1/',
   integrations: [
     tailwind(),
     sitemap(),
