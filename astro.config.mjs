f68dbe83c939018ba2f35fbd2d60a1faef60c982
Jordi Ferrer Orri <9082687+jordiferrero@users.noreply.github.com>
2025-11-23 10:41:51 +0100

    fix again

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,6 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://margohanson.github.io',
-  base: process.env.GITHUB_REPOSITORY ? `/${process.env.GITHUB_REPOSITORY.split('/')[1]}/` : '/websiteV1/',
   integrations: [
     tailwind(),
     sitemap(),
